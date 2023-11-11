a499b867eb9840bfd0a8845ab3c872ede365d2c2
atamuradow.arthur <atamuradow.arthur@gmail.com>
2023-11-11 15:53:36 +0500

    add .env, change updateRates method

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const { updateRates } = require("./services/ratesService");
+require("dotenv").config();
+
+const refreshRate = process.env.REFRESH_RATE || 2000;
 
 const app = express();
 const server = http.createServer(app);
@@ -23,7 +26,7 @@ async function broadcastRates() {
 
 setInterval(async () => {
   await broadcastRates();
-}, 1000);
+}, refreshRate);
 
 server.listen(3000, () => {
   console.log("Server listening on port 3000");
--- src/services/ratesService.js
+++ src/services/ratesService.js
@@ -3,14 +3,29 @@ const { getBinancePrices, getHuobiPrices } = require("../utils/apiUtils");
 let bestRates = {};
 
 async function updateRates() {
+  //getting prices
+
   let [binancePrices, huobiPrices] = await Promise.all([
     getBinancePrices(),
     getHuobiPrices(),
   ]);
+
+  //selecting usdt symbols
   const usdtSymbols = binancePrices
     .filter(({ symbol }) => symbol.endsWith("USDT"))
     .map(({ symbol }) => symbol);
 
+  huobiPrices
+    .filter(({ symbol }) => symbol.endsWith("usdt"))
+    .forEach((price) => {
+      const usdtSymbol = price.symbol.toUpperCase();
+
+      if (!usdtSymbols.includes(usdtSymbol)) {
+        usdtSymbols.push(usdtSymbol);
+      }
+    });
+
+  //selecting best prices
   usdtSymbols.forEach((symbol) => {
     const binancePrice = binancePrices.find(({ symbol: s }) => s === symbol);
 
@@ -18,15 +33,15 @@ async function updateRates() {
       ({ symbol: s }) => s.toLowerCase() === symbol.toLowerCase()
     );
 
-    const huobiRate = huobiPrice ? parseFloat(huobiPrice.close) : -1;
-
     bestRates[symbol] = binancePrice
       ? { price: parseFloat(binancePrice.price), exchange: "Binance" }
       : null;
 
+    const huobiRate = huobiPrice ? parseFloat(huobiPrice.close) : -1;
+
     if (
       huobiPrice &&
-      (!bestRates[symbol] || huobiRate > parseFloat(bestRates[symbol].price))
+      (!binancePrice || huobiRate > parseFloat(binancePrice.price))
     ) {
       bestRates[symbol] = {
         price: parseFloat(huobiPrice.close),
